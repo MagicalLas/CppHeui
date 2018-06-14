8e5f340b9a78225618d61fc3ae037cfde07fbf4a
Las-Wonho <haho013039@gmail.com>
2018-06-14 20:50:42 +0900

    File Read to Hex. I Will Translate to Korean Used UniCode. First, Secend, Last Word Find is Todo

--- CppHeui/CppHeui.cpp
+++ CppHeui/CppHeui.cpp
@@ -2,11 +2,23 @@
 //
 
 #include "CppHeui.h"
-
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
+	const char * filename="F:/CppHeui/main.text";
 	cout << "Hello CMake." << endl;
+	cout << filename << endl;	
+
+	ifstream infile(filename, fstream::in| fstream::out);
+	unsigned int h;
+
+	infile >> hex >> h;
+	cout << h;
+	int a;
+	cin>>a;
 	return 0;
 }
