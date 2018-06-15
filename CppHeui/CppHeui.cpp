784eaca55e7b9088f01fda156ab1bbfd30f506ce
Las-Wonho <haho013039@gmail.com>
2018-06-15 23:53:02 +0900

    Code Seprecated. Some Map Implimantation

--- CppHeui/CppHeui.cpp
+++ CppHeui/CppHeui.cpp
@@ -5,19 +5,63 @@
 #include <iostream>
 #include <fstream>
 #include "windows.h"
-#include "locale.h"
+#include "locale.h" 
 #include <string>
 #include <vector>
 using namespace std;
-//초성 
-static const wchar_t wcHead[] = { L'ㄱ', L'ㄲ', L'ㄴ', L'ㄷ',
-L'ㄸ', L'ㄹ', L'ㅁ', L'ㅂ',
-L'ㅃ', L'ㅅ', L'ㅆ', L'ㅇ',
-L'ㅈ', L'ㅉ', L'ㅊ', L'ㅋ',
-L'ㅌ', L'ㅍ', L'ㅎ' };
-int num_i(wchar_t * s){
+auto HeadFindNum(wchar_t * s) ->int{
 	switch (*s)
 	{
+
+	case L'ㄱ':
+	case L'ㄴ':
+	case L'ㅅ':
+		return 2;
+
+	case L'ㄷ':
+	case L'ㅈ':
+	case L'ㅋ':
+		return 3;
+
+	case L'ㅁ':
+	case L'ㅂ':
+	case L'ㅊ':
+	case L'ㅌ':
+	case L'ㅍ':
+	case L'ㄲ':
+	case L'ㄳ':
+	case L'ㅆ':
+		return 4;
+
+	case L'ㄵ':
+	case L'ㄹ':
+	case L'ㄶ':
+		return 5;
+
+	case L'ㅄ':
+		return 6;
+
+	case L'ㄺ':
+		return 7;
+
+	case L'ㅀ':
+		return 8;
+
+	case L'ㄻ':
+	case L'ㄼ':
+		return 9;
+
+	default:
+		break;
+	}
+	return -1;
+}
+
+auto stackIndices(wchar_t * s) -> int{
+	switch (*s)
+	{
+	case L' ':
+		return 0;
 	case L'ㄱ':
 		return 1;
 	case L'ㄴ':
@@ -32,21 +76,52 @@ int num_i(wchar_t * s){
 		return 6;
 	case L'ㅅ':
 		return 7;
-	case L'ㅇ':
-		return 8;
 	case L'ㅈ':
-		return 9;
+		return 8;
 	case L'ㅊ':
-		return 10;
+		return 9;
 	case L'ㅋ':
-		return 11;
+		return 10;
 	case L'ㅌ':
+		return 11;
+	case L'ㅍ':
 		return 12;
+	case L'ㄲ':
+		return 13;
+	case L'ㄳ':
+		return 14;
+	case L'ㄵ':
+		return 15;
+	case L'ㄶ':
+		return 16;
+	case L'ㄺ':
+		return 17;
+	case L'ㄻ':
+		return 18;
+	case L'ㄼ':
+		return 19;
+	case L'ㄽ':
+		return 20;
+	case L'ㄾ':
+		return 21;
+	case L'ㄿ':
+		return 22;
+	case L'ㅄ':
+		return 23;
+	case L'ㅆ':
+		return 24;
 	default:
 		break;
 	}
-	return 1;
+	return -1;
 }
+
+//초성 
+static const wchar_t wcHead[] = { L'ㄱ', L'ㄲ', L'ㄴ', L'ㄷ',
+L'ㄸ', L'ㄹ', L'ㅁ', L'ㅂ',
+L'ㅃ', L'ㅅ', L'ㅆ', L'ㅇ',
+L'ㅈ', L'ㅉ', L'ㅊ', L'ㅋ',
+L'ㅌ', L'ㅍ', L'ㅎ' };
 
 //중성 
 static const wchar_t wcMid[] = { L'ㅏ', L'ㅐ', L'ㅑ', L'ㅒ',
@@ -63,6 +138,7 @@ L'ㄽ', L'ㄾ', L'ㄿ', L'ㅀ',
 L'ㅁ', L'ㅂ', L'ㅄ', L'ㅅ',
 L'ㅆ', L'ㅇ', L'ㅈ', L'ㅊ',
 L'ㅋ', L'ㅌ', L'ㅍ', L'ㅎ' };
+
 int BreakHan(wchar_t *str, wchar_t *buffer,unsigned int nSize)
 {
 	unsigned int pos = 0;
