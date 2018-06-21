6b56d0ddd1ef0c3e05526f59ac22589d2de8db60
Las-Wonho <haho013039@gmail.com>
2018-06-21 16:59:41 +0900

    Added Interpreter

--- CppHeui/CppHeui.cpp
+++ CppHeui/CppHeui.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <stack>
 using namespace std;
 
 //wchar to int
@@ -155,25 +156,89 @@ struct Char
 	}
 };
 
-struct Storage
+struct Printable
 {
-	int Type;
-	vector<int> Memory;
+	virtual auto toString()->string = 0;
 };
-
-struct Machine
+struct State : Printable
 {
-	vector<Char> CodeSpace; // use like 2d array
-	unique_ptr<Storage> CurrentStorage;
-	int xpos;
-	int ypos;
-	int dx;
-	int dy;
-	bool terminated;
-	int run(wstring code) = delete;
-	vector<Char> initCodespace(wstring code)=delete;
+	int cx;
+	int cy;
+	State() {
+		cx = 0;
+		cy = 0;
+	}
+	auto toString() -> string override {
+		return "STATE : " + to_string(cx) + ", " + to_string(cy);
+	}
 };
+struct CodeInterpreter
+{
+	stack<int> storage;
+	auto run(Char code, State& state) -> void{
 
+		
+	}
+	auto analyseHead(wchar_t head) -> bool {
+		switch (head)
+		{
+		case L'ㅎ':
+			exit(0);
+			break;
+		case L'ㄷ': {
+			auto a = storage.top();
+			storage.pop();
+			auto b = storage.top();
+			storage.pop();
+			storage.push(a + b);
+		}
+		default:
+			break;
+		}
+		return true;
+	}
+	auto analyseMiddle(wchar_t middle, State& state) -> bool{
+		switch (middle)
+		{
+		case L'ㅏ':
+			state.cx += 1;
+			break;
+		case L'ㅑ':
+			state.cx += 2;
+			break;
+		case L'ㅓ':
+			state.cx -= 1;
+			break;
+		case L'ㅕ':
+			state.cx -= 2;
+			break;
+		case L'ㅗ':
+			state.cy += 1;
+			break;
+		case L'ㅛ':
+			state.cy += 2;
+			break;
+		case L'ㅜ':
+			state.cy -= 1;
+			break;
+		case L'ㅠ':
+			state.cy -= 1;
+			break;
+		case L'ㅢ':
+			break;
+		case L'ㅡ':
+			break;
+		case L'ㅣ':
+			break;
+		default:
+			break;
+		}
+		return true;
+	}
+};
+auto print(Printable* object) -> void {
+	cout<<object->toString()<<endl;
+}
 template<class T>
 class TD;
 
@@ -186,23 +251,26 @@ auto wcharToChar(wchar_t word) -> Char& {
 	w.Lead = wcHead[(word - 0xAC00) / (21 * 28)];
 	w.Vowel = wcMid[(word - 0xAC00) % (21 * 28) / 28];
 	w.Tail = wcTail[(word - 0xAC00) % 28];
+
 	return w;
 };
 
 auto init()-> void {
-	cout << "Start of Program" << endl;
+	cout << "Start of Programs" << endl;
 	setlocale(LC_ALL, "Korean");
 };
 
 int main()
 {
 	init();
+	CodeInterpreter interpre;
+	State st;
+	interpre.analyseMiddle(L'ㅏ', st);
+	interpre.analyseHead(L'ㅎ', st);
+	interpre.analyseMiddle(L'ㅠ', st);
+	interpre.analyseMiddle(L'ㅜ', st);
 
-	Machine machine{};
-	wstring str=L"가밣따빠밣밟따뿌";
-	vector<Char> code;
-	code.push_back(wcharToChar(str[7]));
-	wcharToChar(L'\n').print();
+	print(&st);
 
 	string s;
 	cin >> s;
